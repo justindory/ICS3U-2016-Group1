5a6f59be8d7508a768c4cec76bc2c59c50ebaa12
Justin <justin.dory@ocsbstudent.ca>
2017-01-13 14:32:00 -0500

    Added prices to the global class

---
+++ globals.py
@@ -0,0 +1,42 @@
+import main_menu_scene 
+import game_scene 
+import shop_scene
+import gamepick 
+import random
+
+class Globals():
+    #stats
+    global fullhealth
+    fullhealth = 100
+    global playerdmglowest
+    playerdmglowest = 20
+    global playerdmghighest
+    playerdmghighest = 50
+    global playercritchance
+    playercritchance = 7.5
+    global playercritdmg
+    playercritdmg = 150
+    global overtimeregen
+    overtimeregen = 5
+    global playerarmor
+    playerarmor = 2.5
+    global playeratkspeed
+    playeratkspeed = 0.5
+    
+    #coins
+    global coins
+    coins = 0
+    
+    #prices
+    global price1
+    price1 = 100
+    global price2
+    price2 = 100
+    global price3
+    price3 = 100
+    global price4
+    price4 = 100
+    global price5
+    price5 = 100
+    global price6
+    price6 = 100
